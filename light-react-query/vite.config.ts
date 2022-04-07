7d8a0211fee43ddb7a53eb28c3eb71f736482bb2
xuqy01 <xuqy01@mingyuanyun.com>
2022-04-07 11:58:05 +0800

    fix: initialize readme

--- light-react-query/vite.config.ts
+++ light-react-query/vite.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path' // 主要用于alias文件路径别名
+import { resolve } from 'path' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/hooks/index.js'), // 设置入口文件
-      name: 'light-react-query', // 起个名字，安装、引入用
-      fileName: (format) => `light-react-query.${format}.js` // 打包后的文件名
+      entry: resolve(__dirname, 'src/hooks/index.js'), 
+      name: 'light-react-query', 
+      fileName: (format) => `light-react-query.${format}.js`
     },
-    sourcemap: false, // 输出.map文件
+    sourcemap: false, 
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['react'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'React'
         }
